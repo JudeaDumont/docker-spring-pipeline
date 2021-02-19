2f68d3f446f6c09216486bd2d64de6f7535fdb1e
judeadumont <dumontjudea2@gmail.com>
2021-02-18 22:43:47 -0700

    aliasing is unbelievably difficult up to jenkins container

--- SetupMySystem/iterate-entire-environment.sh
+++ SetupMySystem/iterate-entire-environment.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo 'Aliases======================================'
 for key in "${!BASH_ALIASES[@]}"; do
     printf '%s=%q\n' "$key" "${BASH_ALIASES[$key]}"
 done
+
+echo '============================================='
+
 set
 set -e
 ./DockerCommands/stop-all-business-containers.sh || :
