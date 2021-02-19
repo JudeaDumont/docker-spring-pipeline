9b9b390a507e23e98569bfbd9876cda17f7b1469
judeadumont <dumontjudea2@gmail.com>
2021-02-18 23:30:36 -0700

    bash shells execute interactively with aliases!
    -CLEAN UP

--- SetupMySystem/iterate-entire-environment.sh
+++ SetupMySystem/iterate-entire-environment.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo 'Aliases======================================'
-for key in "${!BASH_ALIASES[@]}"; do
-    printf '%s=%q\n' "$key" "${BASH_ALIASES[$key]}"
-done
-alias
-echo '============================================='
-
-set
 set -e
 source DockerCommands/stop-all-business-containers.sh
 
